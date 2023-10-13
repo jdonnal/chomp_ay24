f240cee129b890f2e1928042dff3ccf4c93b7962
John Donnal <35339269+jdonnal@users.noreply.github.com>
2023-10-13 09:39:39 -0400

    basic game with rectangles

--- game.py
+++ game.py
@@ -1,3 +1,18 @@
+import pygame
+import sys
 
-print("Hello world!")
-print("new message!")
+pygame.init()
+
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("Chomp!")
+screen.fill((114, 159, 232))
+
+pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
+pygame.display.flip()
+while True:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            print("thanks for playing!")
+            pygame.quit()
+            sys.exit()
