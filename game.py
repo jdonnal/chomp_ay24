b4280c642865041791d24d6e29f63b4c90a75955
John Donnal <35339269+jdonnal@users.noreply.github.com>
2023-10-18 09:27:31 -0400

    use parameteric variables

--- game.py
+++ game.py
@@ -1,17 +1,31 @@
 import pygame
 import sys
 
+# DIMENSIONS
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = 20
+TILE_SIZE = 64  # tiles are square height==width
+
+# COLORS
+WATER_COLOR = (114, 159, 232)
+SAND_COLOR = (100, 25, 0)
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
-screen.fill((114, 159, 232))
+screen.fill(WATER_COLOR)
+pygame.draw.rect(screen,
+                 SAND_COLOR,
+                 (0, SCREEN_HEIGHT - SAND_HEIGHT,
+                  SCREEN_WIDTH, SAND_HEIGHT))
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(sand, (SCREEN_WIDTH / 2 - TILE_SIZE / 2,
+                   SCREEN_HEIGHT / 2 - TILE_SIZE / 2))
 
-pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
 pygame.display.flip()
 while True:
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             print("thanks for playing!")
             pygame.quit()
